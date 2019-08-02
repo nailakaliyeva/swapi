98cad90e479f0d579bd298f5850564e412aa0849
Naila <nailakaliyeva@gmail.com>
2019-08-02 20:37:31 +0000

    kept the color of yellow if its in favs list

--- src/js/views/people.js
+++ src/js/views/people.js
@@ -13,19 +13,18 @@ export class People extends React.Component {
 							<h1 className="text-center text-info">Peoples names</h1>
 							<ul className="list-group">
 								{store.people.map((e, i) => {
+									let tiger = store.favorites.find(fav => fav.name === e.name);
+									let color = "far fa-star";
+									if (tiger !== undefined) color = "far fa-star text-warning";
+
+									//tiger !== undefined ? "far fa-star text-warning" : "far fa-star";}
+
 									return (
 										<li key={i} className="list-group-item d-flex justify-content-between">
 											{e.name}
+
 											<i
-												className="far fa-star"
-												//{() => {
-												//let obj = store.favorites.find(fav => fav.name === e.name);
-												//let color = "far fa-star";
-												//if (obj !== undefined) color = "far fa-star text-warning";
-												//console.log(color);
-												//return color;
-												// obj !== undefined ? "far fa-star text-warning" : "far fa-star";}
-												//	}
+												className={color}
 												onClick={star => actions.addToFavoritePeople(star, e)}
 											/>
 										</li>
